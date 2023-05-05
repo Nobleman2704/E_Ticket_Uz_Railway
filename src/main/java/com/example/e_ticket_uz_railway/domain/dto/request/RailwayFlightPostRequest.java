c712b66876639e670768a267201a6c60e2fdae92
Nobleman2704 <turayevasad@gmail.com>
2023-05-05 23:05:58 +0500

    Many things have been changed

---
+++ src/main/java/com/example/e_ticket_uz_railway/domain/dto/request/RailwayFlightPostRequest.java
@@ -0,0 +1,17 @@
+package com.example.e_ticket_uz_railway.domain.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class RailwayFlightPostRequest {
+    private String railwayFlightName;
+    private LocalDateTime expirationDate;
+}
